40cab20465eacced53905be113ef9dafec57a677
Lucas Vinicius <lucasvmr6@outlook.com>
2022-11-07 16:11:47 -0300

    criacao do componente Tabela de produtos

---
+++ src/app/component/tabela-produtos/tabela-produtos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-produtos',
+  templateUrl: './tabela-produtos.component.html',
+  styleUrls: ['./tabela-produtos.component.css']
+})
+export class TabelaProdutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
